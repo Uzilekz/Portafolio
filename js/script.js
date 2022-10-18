5db4fce0674f5a3c24eacbee121829044813364b
Uzilekz <perezaguerregerardo2004@gmail.com>
2022-10-18 04:39:34 -0300

    Casi listo

--- js/script.js
+++ js/script.js
@@ -33,27 +33,3 @@ let typingEffect = new Typed(".multiText", {
     backDelay : 2000,
     loop : true
 })
-
-// Deslizar SLIDER de portafolio
-$(".owl-carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000, //1000ms = 1s;
-    autoplayHoverPause: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-        0:{
-            items: 1,
-            nav: true
-        },
-        800:{
-            items: 2,
-            nav: true
-        },
-        1300:{
-            items: 3,
-            nav: true
-        }
-    }
-});
